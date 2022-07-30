c8ce4dd8cd99ecbc0bc029bed8113fc8b12f3b28
Jordan Compton <110277956+thegamersparadox@users.noreply.github.com>
2022-07-30 07:01:09 +0000

    added code

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-git -- version
+import json, requests
+
+apiKey= "367dde354e65a936886c62cd5e38d1fe"
+
+baseURL = "https://api.openweathermap.org/data/2.5/weather"
+
+
+
+print ('Hello, welcome to "Weather 4 U", please input your city now.')
+City = input()
+
+
+
+
+
+URL= f"{baseURL}?q={City}&units=imperial&APPID={apiKey}"
+print (URL)
+
+response=requests.get(URL)
+unformatedData=response.json()
+
+again = 'yes'
+
+while again == 'yes':
+  print (unformatedData)
+  current = unformatedData['main']['temp']
+  max=unformatedData['main']['temp_max']
+  print ('Your current temperature is: ',current)
+  print ('The max tepmerature is: ',max)
+else:
+  print ("Have a nce day.")
