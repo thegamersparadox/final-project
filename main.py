323df783cc9da6dd9c4d26465354b8ed045ab1af
Jordan Compton <110277956+thegamersparadox@users.noreply.github.com>
2022-07-30 07:33:20 +0000

    current preliminary code

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ baseURL = "https://api.openweathermap.org/data/2.5/weather"
 
 
 
-print ('Hello, welcome to "Weather 4 U", please input your city now.')
-City = input()
-
-
-
-
-
-URL= f"{baseURL}?q={City}&units=imperial&APPID={apiKey}"
-print (URL)
-
-response=requests.get(URL)
-unformatedData=response.json()
+print ('Hello, welcome to "Weather 4 U"')
+City=''
 
 again = 'yes'
 
 while again == 'yes':
-  print (unformatedData)
-  current = unformatedData['main']['temp']
-  max=unformatedData['main']['temp_max']
-  print ('Your current temperature is: ',current)
-  print ('The max tepmerature is: ',max)
-else:
-  print ("Have a nce day.")
+  print('please enter your city now:')
+  City= input()
+  URL= f"{baseURL}?q={City}&units=imperial&APPID={apiKey}"
+  response=requests.get(URL)
+  unformatedData=response.json()
+  if response.status_code==200:
+    current = unformatedData['main']['temp']
+    max=unformatedData['main']['temp_max']
+    print ('Your current temperature is: ',current)
+    print ('The max tepmerature is: ',max)
+    print()
+    print('would you like to search for another city? (yes or no)')
+    again= input()
+  else:
+    print ('that is an invalid city, please try again.')
+    
+print ("Have a nice day.")
